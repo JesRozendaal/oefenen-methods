dd3ba2b8774141bf5d251ae05d917bb3b722f209
Jessica Rozendaal <remyenjessica@gmail.com>
2022-01-19 16:22:49 +0100

    oefenen

--- math-methods.js
+++ math-methods.js
@@ -8,18 +8,20 @@ console.log(Math.PI);
 // Bereken de omtrek van een cirkel met een diameter van 4 en log dit in de terminal. De eenheid (cm, meters, etc. maakt niet uit).
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
-
+const omtrek = 4 * Math.PI;
+console.log(omtrek);
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
-
+const roundedOmtrek = Math.round(12 * Math.PI);
+console.log(roundedOmtrek);
 
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
-
+console.log(Math.sqrt(9));
 
 
 /* Opdracht 4: */
@@ -28,3 +30,10 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+const randomNumber = Math.random();
+if (randomNumber >= 0.5) {
+    console.log(randomNumber, "Het glas is halfvol");
+}
+if (randomNumber < 0.5) {
+    console.log(randomNumber, "Het glas is half leeg");
+}
--- string-methods.js
+++ string-methods.js
@@ -13,7 +13,7 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
-
+console.log(fruit.length > vegetable.length);
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
@@ -21,7 +21,13 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
+const lastIndexFruit = fruit.length -1;
+const lastIndexVegetable = vegetable.length -1;
 
+const lastFruit = fruit.substring(lastIndexFruit);
+const lastVegetable = vegetable.substring(lastIndexVegetable);
+
+console.log(lastFruit, lastVegetable);
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
@@ -29,7 +35,9 @@ const vegetable = "Aardappel";
 
 const userInput = "  De kat krabt de krullen van de trap ";
 
+const trimmed = userInput.trim();
 
+console.log(trimmed);
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
@@ -37,13 +45,17 @@ const userInput = "  De kat krabt de krullen van de trap ";
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
 
-
+const check = story.includes("Fantastisch");
+console.log(check);
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
 
+const endOfSentence = story.indexOf(".");
 
+const sentence = story.substring(0, endOfSentence +1);
+console.log(sentence);
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -54,6 +66,8 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 
 const price = "$35.59";
 
+const euroPrice = price.replace("$", "€").replace(".", ",");
+console.log(euroPrice);
 
 
 
